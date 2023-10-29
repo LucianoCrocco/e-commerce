ba7c1109b4245fd7de357ea172a99e5424446fe2
Luciano Agustin Crocco <lucianoacrocco@gmail.com>
2023-10-29 11:09:26 -0300

    Estructura E-Commerce

--- site/src/layout/Layout.tsx
+++ site/src/layout/Layout.tsx
@@ -1,18 +1,12 @@
-import { Button, Grid } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Grid } from "@mui/material";
+import { NavBar } from "../components/NavBar";
+
 
 export function Layout(): JSX.Element {
    return (
       <Grid container direction={'row'}>
-         <Grid item xs={12} component={'nav'}>
-            <Link to={'/'}>
-               <Button variant={'contained'}>
-                  Ir a
-                  Boton 1
-               </Button>
-            </Link>
-         </Grid>
+         <NavBar />
       </Grid>
    )
 }
-
+//TODO: Agregar 
