d4e683f44d9a00246eabb2aaaeac2dd030a9ee39
Luciano Agustin Crocco <lucianoacrocco@gmail.com>
2023-10-31 12:31:37 -0300

    Reestructuracion

--- site/src/router/router.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Layout } from "../layout/Layout";
-
-export const router = createBrowserRouter([
-   {
-      path: '/*',
-      element: <Layout />,
-   }
-]);
